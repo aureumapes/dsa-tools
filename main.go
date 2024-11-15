526de2f7d9e25f97afb7f008db6886edcb2eb58d
AureumApes <janundartur@gmail.com>
2024-11-15 18:46:23 +0100

    Removed unnessesary stuff from router settings

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"github.com/aureumapes/dsa-tools/database"
 	"github.com/aureumapes/dsa-tools/route"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"time"
 )
 
 func main() {
@@ -18,14 +16,6 @@ func main() {
 	database.Db.AutoMigrate(&database.Jubilee{})
 	database.Db.AutoMigrate(&database.Adventure{})
 	database.Db.AutoMigrate(&database.Char{})
-
-	router.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
-		AllowCredentials: false,
-		MaxAge:           12 * time.Hour,
-		AllowOrigins:     []string{"*"},
-	}))
 
 	router.LoadHTMLGlob("resource/static/*html")
 
